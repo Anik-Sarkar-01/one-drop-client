8d9dd4a8c022fa8c442338cbcc848de978ee3bc5
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-05-17 02:21:20 +0600

    all donation request page added

--- src/pages/AdminDashboard/AllDonationRequests/AllDonationRequests.jsx
+++ src/pages/AdminDashboard/AllDonationRequests/AllDonationRequests.jsx
@@ -1,16 +1,81 @@
 import React, { useState } from 'react';
-import useAllDonationRequests from '../../../hooks/useAllDonationRequests';
+import Swal from 'sweetalert2';
 
+import { Link } from 'react-router-dom';
+import useAllDonationRequests from '../../../hooks/useAllDonationRequests';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
+import useAuth from '../../../hooks/useAuth';
+import WelcomeMessage from '../../../components/WelcomeMessage/WelcomeMessage';
 
 const AllDonationRequests = () => {
-    const { allDonationRequests = [] } = useAllDonationRequests();
+    const { allDonationRequests = [], refetch } = useAllDonationRequests();
     const [status, setStatus] = useState('all');
-
-    console.log(allDonationRequests);
-
+    const axiosPublic = useAxiosPublic();
+    const { toastSuccess, toastError } = useAuth();
     const handleFilterChange = (e) => {
         setStatus(e.target.value);
     };
+
+    const handleComplete = async (id) => {
+        const status = {
+            donationStatus: "Done",
+        }
+        const { data } = await axiosPublic.patch(`/change-donation-status/${id}`, status);
+        if (data.modifiedCount > 0) {
+            toastSuccess("Status Updated.");
+            refetch();
+        }
+        else {
+            toastError("Error Occurred.")
+        }
+    }
+
+    const handleCancel = async (id) => {
+        const status = {
+            donationStatus: "Canceled",
+        }
+        const { data } = await axiosPublic.patch(`/change-donation-status/${id}`, status)
+        if (data.modifiedCount > 0) {
+            toastSuccess("Status Updated.");
+            refetch();
+        }
+        else {
+            toastError("Error Occurred.")
+        }
+    }
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const { data } = await axiosPublic.delete(`/donation-requests/${id}`)
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your request has been deleted.",
+                        icon: "success"
+                    });
+                    refetch();
+                }
+                else {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Something went wrong!",
+                    });
+                }
+
+            }
+        });
+    }
 
     const filteredRequests = status === 'all'
         ? allDonationRequests
@@ -20,11 +85,11 @@ const AllDonationRequests = () => {
 
     return (
         <div>
-            <div className="mb-4 flex justify-between items-center">
-                <h2 className="text-xl font-semibold">My Donation Requests ({filteredRequests.length})</h2>
-
-                <div className="flex items-center gap-2">
-                    <label className="font-medium">Filter by Status:</label>
+            <WelcomeMessage heading={`All Donation Requests`} subheading={"Find All The Donation Requests"}></WelcomeMessage>
+            <div className="mb-4 flex justify-between items-center pt-5">
+                <h2 className="text-lg md:text-xl font-semibold">All Donation Requests ({filteredRequests.length})</h2>
+                <div className="flex flex-col md:flex-row items-center gap-2">
+                    <label className="sm:text-sm text-lg font-medium">Filter by Status:</label>
                     <select
                         className="select select-bordered"
                         value={status}
@@ -39,55 +104,69 @@ const AllDonationRequests = () => {
                 </div>
             </div>
 
-            <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
-                <table className="table table-sm">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Recipient Name</th>
-                            <th>Recipient Address</th>
-                            <th>Donation Date</th>
-                            <th>Donation Time</th>
-                            <th>Blood Group</th>
-                            <th>Donation Status</th>
-                            <th>Donor Information</th>
-                            <th>#</th>
-                            <th>#</th>
-                            <th>#</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filteredRequests.map((request, index) => (
-                            <tr key={request._id}>
-                                <th>{index + 1}</th>
-                                <td>{request?.recipientName}</td>
-                                <td>{request?.recipientUpazila}, {request?.recipientDistrict}</td>
-                                <td>{request?.donationDate}</td>
-                                <td>{request?.donationTime}</td>
-                                <td>{request?.bloodGroup}</td>
-                                <td className='flex flex-col items-center gap-2'>
-                                    {request?.donationStatus}
-                                    {request?.donationStatus?.toLowerCase() === 'inprogress' && (
-                                        <div className='flex gap-2'>
-                                            <button className='btn btn-success btn-sm'>Done</button>
-                                            <button className='btn btn-error btn-sm'>Cancel</button>
-                                        </div>
-                                    )}
-                                </td>
-                                <td>
-                                    {request?.donorName}<br />
-                                    {request?.donorEmail}
-                                </td>
-                                <td><button className='btn btn-primary btn-sm'>Edit</button></td>
-                                <td><button className='btn btn-error btn-sm'>Delete</button></td>
-                                <td><button className='btn btn-accent btn-sm'>View</button></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                {filteredRequests.length === 0 && (
-                    <div className="p-4 text-center text-gray-500">No donation requests found for selected filter.</div>
-                )}
+            <div className='space-y-10'>
+
+                <div>
+                    <div className="overflow-x-auto rounded-box border border-gray-300">
+                        <table className="table table-sm">
+                            <thead>
+                                <tr className='bg-red-500 text-white'>
+                                    <th className='py-5'>#</th>
+                                    <th>Recipient Name</th>
+                                    <th>Recipient Address</th>
+                                    <th>Donation Date</th>
+                                    <th>Donation Time</th>
+                                    <th>Blood Group</th>
+                                    <th>Donation Status</th>
+                                    <th>Donor Information</th>
+                                    <th>#</th>
+                                    <th>#</th>
+                                    <th>#</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filteredRequests.map((request, index) => (
+                                    <tr key={request._id}>
+                                        <th>{index + 1}</th>
+                                        <td>{request?.recipientName}</td>
+                                        <td>{request?.recipientUpazila}, {request?.recipientDistrict}</td>
+                                        <td>{request?.donationDate}</td>
+                                        <td>{request?.donationTime}</td>
+                                        <td>{request?.bloodGroup}</td>
+                                        <td className='flex py-8 flex-col justify-center items-center gap-2'>
+                                            <p className='font-semibold'>{request?.donationStatus}</p>
+                                            {
+                                                request?.donationStatus?.toLowerCase() === 'inprogress' && (
+                                                    <div className='flex gap-2'>
+                                                        <button onClick={() => handleComplete(request._id)} className='btn text-white btn-success btn-sm'>Done</button>
+                                                        <button onClick={() => handleCancel(request._id)} className='btn text-white btn-error btn-sm'>Cancel</button>
+                                                    </div>
+                                                )
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                request?.donationStatus?.toLowerCase() === 'inprogress' && (
+                                                    <div className='text-center'>
+                                                        <p className='font-semibold'>{request?.donorName}</p>
+                                                        <p className='font-semibold'>{request?.donorEmail}</p>
+                                                    </div>
+                                                )
+                                            }
+                                        </td>
+                                        <td><Link to={`/dashboard/edit-donation-request/${request?._id}`} className='btn btn-primary text-white btn-sm'>Edit</Link></td>
+                                        <td><button onClick={() => handleDelete(request?._id)} className='btn btn-error text-white btn-sm'>Delete</button></td>
+                                        <td><Link to={`/donation-request-details/${request?._id}`} className='btn btn-accent text-white btn-sm'>View</Link></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+
+                        </table>
+                        {filteredRequests.length === 0 && (
+                            <div className="p-4 text-center text-gray-500">No donation requests found for this selected filter.</div>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/pages/DashboardHome/DashboardHome.jsx
+++ src/pages/DashboardHome/DashboardHome.jsx
@@ -86,7 +86,7 @@ const MyDonationRequest = () => {
 
     return (
         <div className='space-y-10'>
-            <WelcomeMessage heading={`Welcome to the dashboard - ${user?.name}`} subheading={"Find Recent Donation Requests Made by You below"}></WelcomeMessage>
+            <WelcomeMessage heading={`Welcome to the dashboard - ${user?.name}`} subheading={""}></WelcomeMessage>
             {isAdmin ? <>
                 <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5'>
                     <div className="rounded-xl p-7 bg-gradient-to-l from-red-300 to-red-100 flex gap-2 items-center justify-evenly shadow-xl h-40 ">
